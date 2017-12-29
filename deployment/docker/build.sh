e7ca8eff7d8029d02a91f1e92d9291b31140ff0d
root <root@vps-docker-1>
2017-12-30 00:24:49 +0300

    [*] fix missprint

--- deployment/docker/build.sh
+++ deployment/docker/build.sh
@@ -2,5 +2,5 @@
 
 d=`realpath $0`
 cd $d
-#docker build --tag cloudfiles:latest --no-cache ./
-docker build --tag cloudfiles:latest ./
+docker build --tag cloudfiles:latest --no-cache ./
+#docker build --tag cloudfiles:latest ./
